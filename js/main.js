0a0777d99d5cb2921efec8fa49350a8c00fa69b9
Dima121880 <surguladze.dima33@gmail.com>
2024-12-09 02:38:10 +0400

    add 4 work

--- js/main.js
+++ js/main.js
@@ -1,4 +1,149 @@
-import { data } from "./data.js"; //date imoirtis shemotanis dros myfunqcia itisheba
+const data = [
+  {
+    product_id: 29448810,
+    user_id: 2143918,
+    user: "GAME-CLUB",
+    username: "GAME-CLUB",
+    photo: "1011",
+    photos_cnt: 8,
+    photo_ver: 11,
+    vip: 15,
+    color: 0,
+    order_date: "2024-11-15 11:01:27",
+    loc_id: 320,
+    cat_id: 164,
+    special_persons: 0,
+    old_price: "0.00",
+    price: "449.00",
+    discount: 5,
+    currency_id: 3,
+    shop_id: 13793,
+    shop_type_id: 0,
+    adtype_id: 0,
+    pr_type_id: 0,
+    cond_type_id: 2,
+    shop: "GAME-CLUB",
+    offer_price: 0,
+    sell_online: 0,
+    quantity: 5,
+    weight: "0.00",
+    views: 27891,
+    product_code: "",
+    video_url: null,
+    brand_id: 28,
+    model_id: 0,
+    rating: 0,
+    rates_count: 0,
+    group_id: null,
+    available_to_pick_up: 0,
+    available_to_shipping: 0,
+    title: "1-წლიანი გარანტიით.PS4  500gb 2ჯოისტიკით+ 400თამაში",
+    descr:
+      "<p><strong>მაღაზია GAME-CLUB გთავაზობთ:</strong><br/><strong>PS4 (PlayStation 4), 500gb 449ლარი,(400 თამაშიანი აქაუნთი 15ლარი).</strong></p><p><strong>დამატებითი ჯოისტიკი 59ლარი. (1 წელი გარანტია)</strong></p><p><strong>.</strong></p><p><strong>* მოყვება ერთ წლიანი გარანტია...</strong></p><p><strong>* მოქმეედებს უფასო მიტანის სერვიზი. ვაგზავნით რეგიონებშიც</strong></p><p><strong>.</strong></p><p></p>",
+    seo_title:
+      "/teqnika/saTamasho-konsolebikontrolerebi/saTamasho-konsoli/1-wliani-garantiiTPS4--500gb-2joistikiT-400Tamashi",
+    stripped_descr:
+      "მაღაზია GAME-CLUB გთავაზობთ:PS4 (PlayStation 4), 500gb 449ლარი,(400 თამაშიანი აქაუნთი 15ლარი).დამატებითი ჯოისტიკი 59ლარი. (1 წელი გარანტია).* მოყვება ერთ წლიანი გარანტია...* მოქმეედებს უფასო მიტანის სერვიზი. ვაგზავნით რეგიონებშიც.",
+    category_tree: [
+      {
+        cat_id: 1064,
+        title: "ტექნიკა",
+        seo_title: "iyideba-teqnika/",
+      },
+      {
+        cat_id: 4553,
+        title: "სათამაშო კონსოლები/კონტროლერები",
+        seo_title: "teqnika/saTamasho-konsolebikontrolerebi/",
+      },
+      {
+        cat_id: 164,
+        title: "სათამაშო კონსოლი",
+        seo_title: "teqnika/saTamasho-konsolebikontrolerebi/saTamasho-konsoli/",
+      },
+    ],
+    lang_data: {
+      title: "1-წლიანი გარანტიით.PS4  500gb 2ჯოისტიკით+ 400თამაში",
+      descr:
+        "<p><strong>მაღაზია GAME-CLUB გთავაზობთ:</strong><br/><strong>PS4 (PlayStation 4), 500gb 449ლარი,(400 თამაშიანი აქაუნთი 15ლარი).</strong></p><p><strong>დამატებითი ჯოისტიკი 59ლარი. (1 წელი გარანტია)</strong></p><p><strong>.</strong></p><p><strong>* მოყვება ერთ წლიანი გარანტია...</strong></p><p><strong>* მოქმეედებს უფასო მიტანის სერვიზი. ვაგზავნით რეგიონებშიც</strong></p><p><strong>.</strong></p><p></p>",
+      seo_title:
+        "/teqnika/saTamasho-konsolebikontrolerebi/saTamasho-konsoli/1-wliani-garantiiTPS4--500gb-2joistikiT-400Tamashi",
+      stripped_descr:
+        "მაღაზია GAME-CLUB გთავაზობთ:PS4 (PlayStation 4), 500gb 449ლარი,(400 თამაშიანი აქაუნთი 15ლარი).დამატებითი ჯოისტიკი 59ლარი. (1 წელი გარანტია).* მოყვება ერთ წლიანი გარანტია...* მოქმეედებს უფასო მიტანის სერვიზი. ვაგზავნით რეგიონებშიც.",
+      descr_id: 0,
+      product_id: 29448810,
+      lang_id: 4,
+    },
+    photos: [
+      {
+        large:
+          "https://static.my.ge/mymarket/photos/large/1011/29448810_1.jpg?v=11",
+        thumbs:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_1.jpg?v=11",
+        forEdit:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_1.jpg",
+      },
+      {
+        large:
+          "https://static.my.ge/mymarket/photos/large/1011/29448810_2.jpg?v=11",
+        thumbs:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_2.jpg?v=11",
+        forEdit:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_2.jpg",
+      },
+      {
+        large:
+          "https://static.my.ge/mymarket/photos/large/1011/29448810_3.jpg?v=11",
+        thumbs:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_3.jpg?v=11",
+        forEdit:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_3.jpg",
+      },
+      {
+        large:
+          "https://static.my.ge/mymarket/photos/large/1011/29448810_4.jpg?v=11",
+        thumbs:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_4.jpg?v=11",
+        forEdit:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_4.jpg",
+      },
+      {
+        large:
+          "https://static.my.ge/mymarket/photos/large/1011/29448810_5.jpg?v=11",
+        thumbs:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_5.jpg?v=11",
+        forEdit:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_5.jpg",
+      },
+      {
+        large:
+          "https://static.my.ge/mymarket/photos/large/1011/29448810_6.jpg?v=11",
+        thumbs:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_6.jpg?v=11",
+        forEdit:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_6.jpg",
+      },
+      {
+        large:
+          "https://static.my.ge/mymarket/photos/large/1011/29448810_7.jpg?v=11",
+        thumbs:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_7.jpg?v=11",
+        forEdit:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_7.jpg",
+      },
+      {
+        large:
+          "https://static.my.ge/mymarket/photos/large/1011/29448810_8.jpg?v=11",
+        thumbs:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_8.jpg?v=11",
+        forEdit:
+          "https://static.my.ge/mymarket/photos/thumbs/1011/29448810_8.jpg",
+      },
+    ],
+    installment_price: 0,
+    installment_period: 0,
+  },
+];
+
 //  davalba-1
 
 function myfunction() {
@@ -47,5 +192,21 @@ const product = document.querySelector("product-list");
 
 // davaleba==4
 
-let newarr = ["1-წლიანი გარანტიით.PS4  500gb 2ჯოისტიკით+ 400თამაში"];
-document.getElementById("PS4").innerHTML = newarr.map(Math.sqrt);
+// let newarr = ["1-წლიანი გარანტიით.PS4  500gb 2ჯოისტიკით+ 400თამაში"];
+// document.getElementById("PS4").innerHTML = newarr.map(Math.sqrt);
+
+// let newarr = "1-წლიანი გარანტიით.PS4  500gb 2ჯოისტიკით+ 400თამაში";
+// document.getElementById("text").innerHTML =
+//   "1-წლიანი გარანტიით.PS4  500gb 2ჯოისტიკით+ 400თამაში";
+
+// let newarr = "1-წლიანი გარანტიით.PS4  500gb 2ჯოისტიკით+ 400თამაში";
+// document.getElementById("text").innerHTML = newarr;
+
+function newarrfunction() {
+  const para = document.createElement("text");
+  const node = document.createTextNode(
+    "1-წლიანი გარანტიით.PS4  500gb 2ჯოისტიკით+ 400თამაში "
+  );
+  para.appendChild(node);
+  document.getElementById("text").appendChild(para);
+}
